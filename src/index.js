6fdb248ce406120c290b02c8c70f204791a59dd6
Christopher McGovern <chrismcg_@hotmail.com>
2019-06-07 20:57:51 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
 <App />
-  </BrowserRouter>
+  </HashRouter>
   , document.getElementById('root'));
-registerServiceWorker();
