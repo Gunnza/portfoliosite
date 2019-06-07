446cd92fd765c75a9a81f3395b4957b27b2e050c
Christopher McGovern <chrismcg_@hotmail.com>
2019-06-08 00:45:42 +0100

    Updates

---
+++ precache-manifest.abe4a91913baf23511e72c1fd457cbac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1638804a7fe54e32a0c4d2ff84c17c",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "85736195a627e17647d8",
+    "url": "/portfoliosite/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "80e655e01e1b68fcbdd9",
+    "url": "/portfoliosite/static/css/main.f2762d14.chunk.css"
+  },
+  {
+    "revision": "85736195a627e17647d8",
+    "url": "/portfoliosite/static/js/2.47117a59.chunk.js"
+  },
+  {
+    "revision": "80e655e01e1b68fcbdd9",
+    "url": "/portfoliosite/static/js/main.ff1b77c2.chunk.js"
+  },
+  {
+    "revision": "05e3dce6310af3144d0b",
+    "url": "/portfoliosite/static/js/runtime~main.6001a2c1.js"
+  },
+  {
+    "revision": "7719c4edccaf080b077b7995cb9db716",
+    "url": "/portfoliosite/static/media/profilepic.7719c4ed.jpg"
+  }
+]);
